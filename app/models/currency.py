08cf36a06456e6a734e8b3c87f54b4014301eba0
TheZeffyr <aleksvinogradnyi@gmail.com>
2026-03-17 21:23:55 +0300

    refactor(models): Refactor model currency

--- app/models/currency.py
+++ app/models/currency.py
@@ -10,20 +10,19 @@ if TYPE_CHECKING:
 
 
 class Currency(BaseModel):
-    """The currency model for financial transactions.
+    """The currency model for financial transactions
 
     Stores information about supported currencies using the ISO 4217 standard.
 
     Attributes:
-        code (str, 3 chars): Three-letter currency code (ISO 4217)
-        name (str): Full name of the currency
-        symbol (str, 1 char): Currency symbol (₽, $, €, etc.)
+        code (str, 3 chars): Three-letter currency code according to ISO 4217.
+        name (str): Full name of the currency.
+        symbol (str, 1 char): Currency symbol. For currencies without a symbol, an empty string.
     """
 
     code: Mapped[str] = mapped_column(
         CHAR(3),
         unique=True,
-        index=True,
         doc="Three-letter currency code according to ISO 4217"
     )
     name: Mapped[str] = mapped_column(
