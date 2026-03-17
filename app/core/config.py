1548ad7721575ca9625d1f81848de6d152752a24
TheZeffyr <aleksvinogradnyi@gmail.com>
2026-03-17 15:23:27 +0300

    fix(core): Add func to bool str in config

--- app/core/config.py
+++ app/core/config.py
@@ -23,11 +23,23 @@ class Config:
                 f"Required environment variable {var_name} is not set"
             )
         return value.strip()
-
+    
+    @staticmethod
+    def _str_to_bool(value: str) -> bool:
+        """Converts the string to a boolean value.
+        
+        Args:
+            value: str: Value of the convert to string
+        
+        Returns:
+            bool: Boolean variable value
+        """
+        return value.lower() in ('true', '1', 't', 'yes', 'y', 'on')
+    
     @property
     def DB_URL(self):
        return self._get_required("DB_URL")
         
     @property
     def DEBUG(self):
-        return bool(self._get_required("DEBUG"))
+        return self._str_to_bool(self._get_required("DEBUG"))
