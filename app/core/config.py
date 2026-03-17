16f24af76e22ca00a2194be9c615646eeecd18bd
TheZeffyr <aleksvinogradnyi@gmail.com>
2026-03-17 14:26:27 +0300

    feat(core): Add config manager

---
+++ app/core/config.py
@@ -0,0 +1,32 @@
+import os
+
+from dotenv import load_dotenv
+
+
+class Config:
+    """Configuration manager for the application."""
+    @staticmethod
+    def _get_required(var_name: str) -> str:
+        """Get required environment variable
+
+        Args:
+            var_name (str): Name of the environment variable.
+
+        Raises:
+            ValueError: If variable is not set.
+
+        Returns:
+            str: Value of the environment variable.
+        """
+        value = os.getenv(var_name)
+
+        if value is None:
+            raise ValueError(
+                f"Required environment variable {var_name} is not set"
+            )
+        return value.strip()
+
+    load_dotenv()
+
+    DB_URL = _get_required("DB_URL")
+    DEBUG = _get_required("DEBUG")
