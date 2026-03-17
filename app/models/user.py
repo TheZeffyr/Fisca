0b6c6fbc5f83dcfb6b35ee90e15173d543f50f6a
TheZeffyr <aleksvinogradnyi@gmail.com>
2026-03-17 16:21:04 +0300

    feat(models): Add user model

---
+++ app/models/user.py
@@ -0,0 +1,46 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import BigInteger, ForeignKey
+
+from app.models import BaseModel
+
+if TYPE_CHECKING:
+    from app.models import (
+        Currency,
+        Category,
+        Saving,
+        Transaction,
+        Account
+    )
+
+
+class User(BaseModel):
+    """User model representing a Telegram bot user.
+    
+    User can:
+		Create categories, accounts, coins, transactions
+    
+    Attributes:
+        tg_id (int): Telegram User ID (unique)
+        currency_id (int): User’s currency id
+    """
+
+    tg_id: Mapped[int] = mapped_column(
+        BigInteger,
+        unique=True,
+        index=True,
+        doc="Telegram user ID from Telegram API"
+    )
+    currency_id: Mapped[int] = mapped_column(
+        ForeignKey("currencies.id", ondelete="RESTRICT"),
+        doc="IUser’s currency id"
+    )
+
+    currency: Mapped["Currency"] = relationship(back_populates="users")
+    categories: Mapped[list["Category"]] = relationship(back_populates="users")
+    savings: Mapped[list["Saving"]] = relationship(back_populates="users")
+    accounts: Mapped[list["Account"]] = relationship(back_populates="users")
+    transactions: Mapped[list["Transaction"]] = relationship(
+        back_populates="users"
+    )
