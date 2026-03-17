3f7bf1454cefc8acd3f4bf102c215ec2061205e9
TheZeffyr <aleksvinogradnyi@gmail.com>
2026-03-17 14:34:23 +0300

    feat(main): Add init config

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import logging
-from app.core import logger
+
+from app.core.logger import setup_logging, get_logger
+from app.core.config import Config
 
 def main():
-    logger.setup_logging(logging.DEBUG)
+    setup_logging(logging.DEBUG)
+    logger = get_logger(__name__)
+
+    try:
+        config = Config()
+    except ValueError as e:
+        logger.critical(f"Error configuration: {e}")
+        raise SystemExit(1)
+    
 
 if __name__ == "__main__":
     main()
