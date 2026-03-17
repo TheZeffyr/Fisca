727c6137e010adb0aa538d5609d6055ad72bef89
TheZeffyr <aleksvinogradnyi@gmail.com>
2026-03-17 14:47:20 +0300

    fix(main): Add load env and set level to logging

--- main.py
+++ main.py
@@ -3,15 +3,22 @@ import logging
 from app.core.logger import setup_logging, get_logger
 from app.core.config import Config
 
+from dotenv import load_dotenv
+
 def main():
     setup_logging(logging.DEBUG)
     logger = get_logger(__name__)
 
     try:
+        load_dotenv()
         config = Config()
     except ValueError as e:
         logger.critical(f"Error configuration: {e}")
         raise SystemExit(1)
+    
+    setup_logging(
+        int(config.DEBUG)
+    )
     
 
 if __name__ == "__main__":
