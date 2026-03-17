b3b03361c5f08b931eebecdedb51ac8023ce7e7b
TheZeffyr <aleksvinogradnyi@gmail.com>
2026-03-17 21:04:02 +0300

    fix(models): Deletes Saving model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,12 +2,13 @@ from .base import BaseModel
 from .user import User
 from .currency import Currency
 from .category import Category
-
+from .saving import Saving
 
 
 __all__ = [
 	"BaseModel",
 	"User",
 	"Currency",
-	"Category"
+	"Category",
+	"Saving",
 ]
